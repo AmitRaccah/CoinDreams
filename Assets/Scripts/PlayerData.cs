0e31b46f979b8ecd0010d48d90ea0bccfc24a9ed
Amit Raccah <amitraccah1990@gmail.com>
2026-01-19 02:00:34 +0200

    PlayFab

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,10 @@
+// using UnityEngine;
+
+// [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
+// public class PlayerDataConfig : ScriptableObject
+// {
+//     public int startingCurrency;
+//     public int startingEnergy;
+//     public int maxEnergy;
+//     public int rollCost;
+// }
